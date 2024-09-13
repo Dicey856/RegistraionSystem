c9af92685fa00a76278cfa45335ce0749ed43360
Dicey <100703818+Dicey856@users.noreply.github.com>
2024-09-13 15:50:41 +0300

    Add files via upload

---
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthSystem.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    public string Name {  get; set; }
+
+    [Required]
+    public string IsoCode { get; set; }
+
+    [Required]
+    [Phone]
+    public string Telephone { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Address { get; set; }
+}
+
